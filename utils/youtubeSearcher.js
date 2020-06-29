7154d00b78eb10d7889f08a4ecb31fbb0a8f5cd8
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-06-29 23:38:29 +0300

    youtube music searcher

---
+++ utils/youtubeSearcher.js
@@ -0,0 +1,27 @@
+const { validateURL } = require('ytdl-core')
+const youtube = require('simple-youtube-api')
+
+const YouTube = new youtube(config.get('YOUTUBE_TOKEN'))
+
+const youtubeSearcher = async args => {
+	let url = args[0]
+	let title = ''
+	if (!urlChecker(args[0])) {
+		let request = ''
+		args.forEach(arg => request += `${arg} `)
+
+		await YouTube.searchVideos(request, 1)
+			.then(results => {
+				url = results[0].url
+				title = results[0].title
+			})
+	}
+
+	return { url, title }
+}
+
+const urlChecker = url => {
+	return validateURL(url)
+}
+
+module.exports = youtubeSearcher
